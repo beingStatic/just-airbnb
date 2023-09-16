765e9d05905fc4de5ebc4bbebd9a91f81ff22a86
beingStatic <mohammedsufiyan23@gmail.com>
2023-09-16 22:06:35 +0530

    nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-4xl font-bold'>NEVER SETTLE!!!</h1>
+      <h1 className='text-4xl font-bold'></h1>
     </main>
   )
 }
