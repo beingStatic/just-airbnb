251dbeaa45b9805e0af8567f877a4e869a03e8e1
beingStatic <mohammedsufiyan23@gmail.com>
2023-09-18 23:40:22 +0530

    Modal Interface

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal />
+        <Modal isOpen />
         <Navbar />{children}</body>
     </html>
   )
