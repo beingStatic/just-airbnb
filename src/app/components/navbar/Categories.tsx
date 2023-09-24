fc5b75fb8bef32f67b979504a49338b03e7417d3
beingStatic <mohammedsufiyan23@gmail.com>
2023-09-25 01:12:09 +0530

    Categories List

--- src/app/components/navbar/Categories.tsx
+++ src/app/components/navbar/Categories.tsx
@@ -1,7 +1,11 @@
 import Container from "../Container";
 import {TbBeach} from "react-icons/tb"
-import {GiWindmill} from "react-icons/gi"
+import {GiBoatFishing, GiWindmill,GiIsland, GiCastle, GiForestCamp, GiCactus,GiCaveEntrance, GiBarn} from "react-icons/gi"
 import { MdOutlineVilla } from "react-icons/md"
+import { TbMountain,TbPool } from "react-icons/tb"
+import { FaSkiing } from "react-icons/fa"
+import { BsSnow } from "react-icons/bs"
+import { IoDiamond } from "react-icons/io5"
 import CategoryBox from "../CategoryBox";
 
 export const categories = [
@@ -20,6 +24,72 @@ export const categories = [
     icon: MdOutlineVilla,
     description:"This property is Modern"
   },
+  {
+    label: "Countryside",
+    icon: TbMountain,
+    description:"This property is Modern"
+  },
+  {
+    label: "Pools",
+    icon: TbPool,
+    description:"This property has a pool"
+  },
+  {
+    label: "Islands",
+    icon: GiIsland,
+    description:"This property is on a island"
+  },
+  {
+    label: "Lake",
+    icon: GiBoatFishing,
+    description:"This property is close to a lake"
+  },
+  {
+    label: "Skiing",
+    icon: FaSkiing,
+    description:"This property has skiing activities"
+  },
+  {
+    label: "Castles",
+    icon: GiCastle,
+    description:"This property is in a castle"
+  },
+  {
+    label: "Camping",
+    icon: GiForestCamp,
+    description:"This property has camping activities"
+  },
+  {
+    label: "Arctic",
+    icon: BsSnow,
+    description:"This property has camping activities"
+  },
+  {
+    label: "Cave",
+    icon: GiCaveEntrance,
+    description:"This property is in a cave"
+  },
+  {
+    label: "Desert",
+    icon: GiCactus,
+    description:"This property is in a desrt"
+  },
+  {
+    label: "Barns",
+    icon: GiBarn,
+    description:"This property is in a barn"
+  },
+  {
+    label: "Desert",
+    icon: GiCactus,
+    description:"This property is in a cave"
+  },
+  {
+    label: "Lux",
+    icon: IoDiamond,
+    description:"This property is luxurious"
+  },
+
 ]
 const Categories = () => {
   return (<>
