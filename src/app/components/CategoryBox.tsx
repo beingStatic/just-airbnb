7db5e5c76e0c16b2ab5175b55655186242e30f78
beingStatic <mohammedsufiyan23@gmail.com>
2023-09-26 17:44:02 +0530

    Category Functionality

--- src/app/components/CategoryBox.tsx
+++ src/app/components/CategoryBox.tsx
@@ -1,19 +1,36 @@
 'use client'
 
 import { IconType } from "react-icons"
-interface CategoryProps{
+interface CategoryBoxProps{
   label: String,
   icon: IconType,
   selected?:boolean
 }
 
-const CategoryBox: React.FC<CategoryProps> = ({
+const CategoryBox: React.FC<CategoryBoxProps> = ({
   label,
-  icon,
+  icon:Icon,
   selected
 }) => {
   return (<>
-  
+    <div className={`
+            flex
+            flex-col
+            items-center
+            justify-center
+            gap-2
+            p-3
+            border-b-2
+            hover:text-neutral-800
+            transition
+            cursor-pointer
+            ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+            ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+            `} 
+    >
+      <Icon size={26} />
+      working
+  </div>
   </>);
 }
  
--- src/app/components/navbar/Categories.tsx
+++ src/app/components/navbar/Categories.tsx
@@ -95,21 +95,24 @@ const Categories = () => {
   return (<>
     <Container>
       <div
-        className="pt-4 
-                  flex
-                  flex-row
-                  items-center
-                  justify-between
-                  overflow-x-auto">
-        
+        className="
+            pt-4 
+            flex
+            flex-row
+            items-center
+            justify-between
+            no-scrollbar
+            overflow-x-auto"
+      >
+            {categories.map(item => {
+          return <CategoryBox 
+              key={item.label}
+              label={item.label}
+              icon={item.icon}
+              // description={item.description}
+              /> })} 
       </div>
-      {categories.map(item => {
-       return <CategoryBox 
-          key={item.label}
-          label={item.label}
-          icon={item.icon}
-          // description={item.description}
-          /> })} 
+      
   </Container>
   </>);
 }
